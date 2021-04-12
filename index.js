cafac21614b202d1e30e246073df61d344e08507
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-12 08:58:06 -0400

    commit tree 3a08e28449a44c165ec277f3238d87e4f46f5010

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+//document.addEventListener('DOMContentLoaded', (e) => {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+        };
+        
+        let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+        };
+
+   
+        
+        
+        function submitData (name, email){
+            formData['name'] = name;
+            formData['email'] = email;
+            //console.log(formData.name, formData.email)
+             let doc = fetch("http://localhost:3000/users", configObj)
+                .then(function(response) {
+                  return response.json();
+                })
+                .then(function(object) {
+                  document.append(object.Name)
+        
+                })
+                .catch(function(error) {
+                  document.append(error.message)
+                }); 
+                return doc
+        }
+        
+ 
+
+//submitData("steven", "seteve@gmail.com");
